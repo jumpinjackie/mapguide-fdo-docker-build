ed2522571c37a37991120f7958149a6e1d0f536b
Jackie Ng <jumpinjackie@gmail.com>
2020-08-24 22:35:36 +1000

    Pass --nochown and --keep-umask to preserve existing FS permissions (needed for httpd to function properly)
    Fixes #20

--- make_installer.sh
+++ make_installer.sh
@@ -7,7 +7,7 @@ DISTRO=${DISTRO_NOVER}${DISTRO_VER}
 INSTALLER_LABEL="MapGuide Open Source 4.0 Preview 2"
 . ./fdo_version.sh
 . ./mapguide_version.sh
-echo "Prepare installer staging area"
+echo "Prepare installer staging area (${DISTRO})"
 mkdir -p staging/fdo-${FDO_VER_TRIPLE}
 mkdir -p staging/mapguideopensource-${MG_VER_TRIPLE}
 echo "Extract tarball contents to staging area"
@@ -16,6 +16,6 @@ tar -zxf artifacts/mapguideopensource-${MG_VER}-${DISTRO}-amd64.tar.gz -C stagin
 cp templates/distros/${DISTRO_NOVER}/install.sh staging/
 chmod +x staging/install.sh
 echo "Create self-extracting archive"
-~/makeself/makeself.sh --xz --quiet --needroot --sha256 staging/ mapguideopensource-${MG_VER}-${DISTRO}-install.run "${INSTALLER_LABEL}" ./install.sh
+~/makeself/makeself.sh --quiet --needroot --nochown --keep-umask --sha256 staging/ mapguideopensource-${MG_VER}-${DISTRO}-install.run "${INSTALLER_LABEL}" ./install.sh
 rm -rf staging/
 mv *.run artifacts/
