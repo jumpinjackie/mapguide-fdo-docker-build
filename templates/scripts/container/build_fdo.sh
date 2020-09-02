b336133f324a82cf5668e8a8e4afdfcc7b80f8b9
Jackie Ng <jumpinjackie@gmail.com>
2020-09-02 11:08:04 +1000

    Build KingOracle provider for the main FDO build script, sourcing the Instant Client SDK from our volume-mounted container path. Fixes #25

--- templates/scripts/container/build_fdo.sh
+++ templates/scripts/container/build_fdo.sh
@@ -10,10 +10,14 @@ THIRDPARTY_BUILD_DIR=/tmp/work/build_area/fdo_thirdparty
 BUILD_DIR=/tmp/work/build_area/fdo
 SRC_DIR=/tmp/work/src
 ARTIFACTS_DIR=/tmp/work/artifacts
+SDKS_DIR=/tmp/work/sdks
 cd $SRC_DIR || exit
 # Our standard default is to assume all thirdparty deps exist as system packages
 ./cmake_bootstrap.sh --working-dir $THIRDPARTY_BUILD_DIR --build 64 --with-ccache
-./cmake_build.sh --fdo-ver-major ${FDO_VER_MAJOR} --fdo-ver-minor ${FDO_VER_MINOR} --fdo-ver-rel ${FDO_VER_REL} --fdo-ver-rev ${FDO_VER_REV} --build 64 --thirdparty-working-dir $THIRDPARTY_BUILD_DIR --cmake-build-dir $BUILD_DIR --with-sdf --with-shp --with-sqlite --with-ogr --with-gdal --with-wfs --with-wms --with-genericrdbms --ninja
+# Oracle Instant Client SDK setup
+export ORACLE_SDK_HOME=$SDKS_DIR/oracle/x64/instantclient_12_2/sdk
+# Main build
+./cmake_build.sh --fdo-ver-major ${FDO_VER_MAJOR} --fdo-ver-minor ${FDO_VER_MINOR} --fdo-ver-rel ${FDO_VER_REL} --fdo-ver-rev ${FDO_VER_REV} --build 64 --thirdparty-working-dir $THIRDPARTY_BUILD_DIR --cmake-build-dir $BUILD_DIR --with-sdf --with-shp --with-sqlite --with-ogr --with-gdal --with-wfs --with-wms --with-genericrdbms --with-kingoracle --with-oci-version 120 --ninja
 cd $BUILD_DIR || exit
 cmake --build . --target package
 mv fdosdk*.tar.gz $ARTIFACTS_DIR
