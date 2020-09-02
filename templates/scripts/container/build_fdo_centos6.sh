5067f0bb27ba4bc11f22d7d069e1b40076983002
Jackie Ng <jumpinjackie@gmail.com>
2020-09-03 02:30:28 +1000

    Remove mysql-devel and postgresql-devel from centos6 image and build against custom mariadb connector for centos6

--- templates/scripts/container/build_fdo_centos6.sh
+++ templates/scripts/container/build_fdo_centos6.sh
@@ -18,10 +18,31 @@ cd $SRC_DIR || exit
 ./cmake_bootstrap.sh --working-dir $THIRDPARTY_BUILD_DIR --all-internal --build 64 --with-ccache
 # For CentOS-based distros, we build against MySQL/PostgreSQL in /sdks
 # Setting MYSQL_DIR is enough for FindMySQL.cmake to pick it up
-export MYSQL_DIR=$SDKS_DIR/mysql/x64
 export ORACLE_SDK_HOME=$SDKS_DIR/oracle/x64/instantclient_12_2/sdk
 PG_BUILD_ROOT=/tmp/work/build_area/pgbuild
+MARIADB_BUILD_ROOT=/tmp/work/build_area/mariadb
 OPENSSL_LOCAL_DIR=/tmp/work/build_area/fdo_thirdparty/Thirdparty/openssl/_install
+# Build MariaDB client if required
+if [ ! -f /usr/local/lib/mariadb/libmariadbclient.a ]; then
+    if [ ! -d $MARIADB_BUILD_ROOT ]; then
+        mkdir -p $MARIADB_BUILD_ROOT
+        echo "Extracting MariaDB tarball"
+        tar -zxf $SDKS_DIR/mariadb-connector-c-3.1.9-src.tar.gz -C $MARIADB_BUILD_ROOT
+    fi 
+    cd $MARIADB_BUILD_ROOT/mariadb-connector-c-3.1.9-src || exit
+    if [ -d ../_build ]; then
+        rm -rf ../_build
+    fi
+    mkdir -p ../_build
+    cd ../_build || exit
+    # The FindOpenSSL.cmake in CMake 2.8 is not flexible enough to pick up our custom built OpenSSL so 
+    # we have to invasively set all the "private" variables that this module looks for up front
+    #
+    # Also, possibly because of this, we need to manually set -pthread/-lpthread to avoid a linker error
+    LIBS="-lpthread" CFLAGS="-pthread" cmake ../mariadb-connector-c-3.1.9-src -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_SSL=OPENSSL -D_OPENSSL_INCLUDEDIR=/tmp/work/build_area/fdo_thirdparty/Thirdparty/openssl/_install/include -D_OPENSSL_LIBDIR=/tmp/work/build_area/fdo_thirdparty/Thirdparty/openssl/_install/lib -D_OPENSSL_VERSION=1.1.1
+    make && make install
+fi
+# Build PostgreSQL client if required
 if [ ! -f /usr/local/pgsql/lib/libpq.a ]; then
     if [ ! -d $PG_BUILD_ROOT ]; then
         mkdir -p $PG_BUILD_ROOT
@@ -42,7 +63,7 @@ if [ ! -f /usr/local/pgsql/lib/libpq.a ]; then
 fi
 # Now for the main build
 cd $SRC_DIR || exit
-./cmake_build.sh --fdo-ver-major ${FDO_VER_MAJOR} --fdo-ver-minor ${FDO_VER_MINOR} --fdo-ver-rel ${FDO_VER_REL} --fdo-ver-rev ${FDO_VER_REV} --build 64 --thirdparty-working-dir $THIRDPARTY_BUILD_DIR --cmake-build-dir $BUILD_DIR --with-sdf --with-shp --with-sqlite --with-ogr --with-gdal --with-wfs --with-wms --with-genericrdbms --with-mysql-static --with-libpq-static --with-kingoracle --with-oci-version 120 --ninja
+./cmake_build.sh --fdo-ver-major ${FDO_VER_MAJOR} --fdo-ver-minor ${FDO_VER_MINOR} --fdo-ver-rel ${FDO_VER_REL} --fdo-ver-rev ${FDO_VER_REV} --build 64 --thirdparty-working-dir $THIRDPARTY_BUILD_DIR --cmake-build-dir $BUILD_DIR --with-sdf --with-shp --with-sqlite --with-ogr --with-gdal --with-wfs --with-wms --with-genericrdbms --with-mariadb-static --with-libpq-static --with-kingoracle --with-oci-version 120 --ninja
 cd $BUILD_DIR || exit
 cmake --build . --target package
 mv fdosdk*.tar.gz $ARTIFACTS_DIR
