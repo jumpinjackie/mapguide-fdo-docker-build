d1a8e2a26c3388b244f3dc4da895dd16e0302f1a
Jackie Ng <jumpinjackie@gmail.com>
2020-10-05 20:02:36 +1100

    Add artifact cleaning script

---
+++ clean_artifacts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf artifacts/Release/*.tar.gz
+rm -rf artifacts/Release/*.run
+rm -rf artifacts/Release/ubuntu*
+rm -rf artifacts/Debug/*.tar.gz
+rm -rf artifacts/Debug/*.run
+rm -rf artifacts/Debug/ubuntu*
