a27f998316c1ea28b59dd849d50acaf968ea9fe0
Jackie Ng <jumpinjackie@gmail.com>
2024-08-05 20:10:35 +1000

    #44: Disable centos/generic distros until we find a suitable replacement

--- make_installer_all.sh
+++ make_installer_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./make_installer.sh centos 7 Release
+#./make_installer.sh centos 7 Release
 ./make_installer.sh ubuntu 22 Release
-./make_installer.sh centos 7 Debug
+#./make_installer.sh centos 7 Debug
 ./make_installer.sh ubuntu 22 Debug
